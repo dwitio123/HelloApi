300e4a1819c7a7e01f8e4760b96ab283c9a5d81a
Tio <tiothebook@gmail.com>
2026-07-20 14:55:11 +0700

    feat: learn model validation

--- DTOs/CreateProductDto.cs
+++ DTOs/CreateProductDto.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HelloApi.DTOs;
 
 public class CreateProductDto
 {
+    [Required(ErrorMessage = "Nama produk wajib diisi.")]
+    [StringLength(100, MinimumLength = 3)]
     public string Name { get; set; } = "";
+
+    [Range(1000, 100000000)]
     public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "Deskripsi produk wajib diisi.")]
+    [StringLength(300, MinimumLength = 10)]
+    public string Description { get; set; } = "";
 }
--- obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2abec43929ba312e3b11794b3a39f7b178c26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b39ed1e396086e68935a483b00ca234e62c8bb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
