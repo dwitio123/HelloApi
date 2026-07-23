22740cd0873554f63fc09a92c8557ec4d40e261c
Tio <tiothebook@gmail.com>
2026-07-23 20:39:34 +0700

    feat: learn EF Core migrations & build database

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using HelloApi.Repositories;
 using HelloApi.Services;
+using HelloApi.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
--- obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07be740654665f2aa1435fd735a0246cb9df1d93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e702941fbee37059086238a09c43811ab8c06378")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
