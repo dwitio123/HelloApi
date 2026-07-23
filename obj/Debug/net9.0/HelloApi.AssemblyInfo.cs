e702941fbee37059086238a09c43811ab8c06378
Tio <tiothebook@gmail.com>
2026-07-23 13:59:05 +0700

    feat: learn EF Core & DbContext

--- obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9cc9e23af6c349bda39b532cfd28593a5ef141")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07be740654665f2aa1435fd735a0246cb9df1d93")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
